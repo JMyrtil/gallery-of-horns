8ab06ac4a0584685b2122464d2144fd97e9960dc
Joel Myrtil <myrtil.joel@gmail.com>
2023-02-27 18:44:26 -0800

    class 1

---
+++ src/Moose.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Moose extends React.Component {
+
+  render() {
+    return (
+<article>
+  <h2>The Mighty Moose</h2>
+  <img src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcSAZJ5Gr5bhF2AsM5kb9q7-wyedkpKrOcVBJgyU6sRieMbXciPX3_gfxNNSgSPZgUpJ2143eZk8Cza2GZ4" alt="" title="Antler"></img>
+  <p>Moose are the largest members of the deer family, standing six feet (1.8 meters) tall from hoof to shoulder, and weighing in at more than 1,000 pounds (450 kilograms). Each of their light to dark brown hairs is hollow, and the air trapped inside provides insulation. A flap of skin called a dewlap hangs from the throat. Males are distinguished from females by their antlers, which grow up to six feet across.</p>
+</article>
+    )
+  };
+}
+
+export default Moose;
---
+++ src/Rhino.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Rhino extends React.Component {
+
+  render() {
+    return (
+<article>
+  <h2>The Remarkable Rhino</h2>
+  <img src="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcTPJNqH_QuQmLvhl9zD9EIayG0ifP89PhMbGvbMmZXkzecnbv9Yo_gd95bf7Ylr42BD-hod9476OPNrkD4" alt="" title="poke"></img>
+  <p>Rhinos once roamed many places throughout Europe, Asia, and Africa and were known to early Europeans who depicted them in cave paintings. At the beginning of the 20th century, 500,000 rhinos roamed Africa and Asia. By 1970, rhino numbers dropped to 70,000, and today, around 27,000 rhinos remain in the wild. Very few rhinos survive outside national parks and reserves due to persistent poaching and habitat loss over many decades. Three species of rhino—black, Javan, and Sumatran—are critically endangered. Today, a small population of Javan rhinos is found in only one national park on the northern tip of the Indonesian island of Java.</p>
+</article>
+    )
+  };
+}
+
+export default Rhino;
