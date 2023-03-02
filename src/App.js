b28f1bd3a1f6ff1970f1021a93cd7c1340d4902a
Joel Myrtil <myrtil.joel@gmail.com>
2023-03-02 08:31:25 -0800

    class 4 lab

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Main from './Main';
 import data from './data.json';
 import Modal from 'react-bootstrap/Modal';
+import ListGroup from "react-bootstrap/ListGroup";
 import './App.css';
 // import { ModalBody } from "react-bootstrap";
 
@@ -11,8 +12,11 @@ class App extends React.Component {
     this.state = {
       hearts: '',
       showModal: false,
-      ani: {}
+      ani: {},
+      howToSort: parseInt(''),
+      filteredData: data
     };
+
   }
 
   addHearts = () => {
@@ -34,17 +38,66 @@ class App extends React.Component {
     });
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // let filter = e.target.filter.value
+    // console.log(filter);
+    // this.setState({
+    //   howToSort: filter
+    // })
+    if (this.state.howToSort === 1) {
+      let newData = data.filter(animal => animal.horns === this.state.howToSort);
+      this.setState({filteredData: newData})
+      console.log(this.setState({filteredData: newData}));
+    } else if (this.state.howToSort === 2) {
+      let newData = data.filter(animal => animal.horns === this.state.howToSort);
+      this.setState({filteredData: newData})
+    } else if (this.state.howToSort === 3) {
+      let newData = data.filter(animal => animal.horns === this.state.howToSort);
+      this.setState({filteredData: newData})
+    }
+  }
+
+
+  howSort = (e) => {
+    // console.log(e.target.value)
+    this.setState({
+      howToSort: parseInt(e.target.value)
+    })
+  }
+
   render() {
+
+    // let aniFilter = data.map((num, idx) => <ListGroup.Item key={idx}>{num}</ListGroup.Item>)
+
     return (
       <>
         <header>
           <h1>The Animal Kingdom</h1>
         </header>
+        <main />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input></input>
+          </label>
+          <label>
+            <select name="filter" onChange={this.howSort}>
+              <option value="1">1 horn</option>
+              <option value="2">2 horn</option>
+              <option value="3">3 horn</option>
+            </select>
+          </label>
+          <button type="submit">Filter</button>
+        </form>
+        <ListGroup>
+          {/* {aniFilter} */}
+        </ListGroup>
         <Main
-          data={data}
+          data={this.state.filteredData}
           addhearts={this.addHearts}
           handleOpenModal={this.handleOpenModal}
         />
+        <main />
         <footer>&copy; by Joel Myrtil</footer>
         <Modal
           show={this.state.showModal}
@@ -54,7 +107,7 @@ class App extends React.Component {
             <img
               src={this.state.ani.imageURL}
               width={465} height={500}
-              alt={this.state.ani.name}/>
+              alt={this.state.ani.name} />
           </Modal.Body>
         </Modal>
       </>
